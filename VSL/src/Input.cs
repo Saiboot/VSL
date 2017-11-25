77b28a7ee48e91cdcd044d5f1b7ec92375a60077
Saiboot <T3eG3e@gmail.com>
2017-11-25 06:15:46 +0100

    Input Module/Handler - Keyboard - Mouse position & buttons

---
+++ VSL/src/Input.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Input;
+
+namespace VSL.Input
+{
+    class InputModule
+    {
+        public InputModule()
+        {
+            m_Keyboard = new KeyboardState();
+            m_Mouse = new MouseState();
+        }
+
+        /// <summary>
+        /// Pull next Input Event.
+        /// </summary>
+        public void Pull_Events()
+        {
+            m_Keyboard = Keyboard.GetState();
+            m_Mouse = Mouse.GetState();
+        }
+
+        /// <summary>
+        ///     <para>
+        ///         Check whether or not a specific Key is being pressed.
+        ///     </para>
+        /// </summary>
+        /// <param name="key">Monogame Key index</param>
+        /// <returns>Current state of Key</returns>
+        public bool KeyState(Keys key)
+        {
+            return m_Keyboard.IsKeyDown(key) ? true : false;
+        }
+
+        /// <summary>
+        ///     <para>
+        ///         Fetch current Mouse position.
+        ///     </para>
+        ///     - Mouse position relative to monitor
+        /// </summary>
+        /// <returns>Current state of Mouse</returns>
+        public Point MousePos()
+        {
+            return m_Mouse.Position;
+        }
+
+        KeyboardState m_Keyboard;
+        MouseState m_Mouse;
+    }
+}
